30918f6ab5124248a71597398a4a2e2f476f93b7
abnercezar <102832541+abnercezar@users.noreply.github.com>
2024-11-30 12:57:18 -0300

    adiciona a Entry Page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
+# Função para converter conteúdo Markdown em HTML
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    if content is None:
+        return None
+    return Markdown().convert(content)
 
+# View para a página inicial
 def index(request):
+
+    entries = util.list_entries()
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
+# View para exibir uma entrada específica da enciclopédia
+def entry(request, title):
+    html_content = convert_md_to_html(title)
+    if html_content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "A entrada solicitada não foi encontrada."
+        })
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": html_content
+    })
